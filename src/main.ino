52bc8124562eab0ee970ed5d4066482154ed47a5
eevee71 <oliwka.pazdro@gmail.com>
2025-05-15 20:40:26 +0200

    first version voltage simulation

--- src/main.ino
+++ src/main.ino
@@ -4,16 +4,16 @@
 
 GamepadController gamepad;
 
-// --- MCP4231 setup ----------------------------------------------------
+// --- MCP4231 
 const byte ADDR_P0 = 0b00000000;
 const byte ADDR_P1 = 0b00010000;
 
-// hardware‑SPI (MCP #0)
+// MCP 0
 const int CS0 = 10;
 #define POT0_IN A0
 #define POT1_IN A1
 
-// soft‑SPI (bit‑bang) (MCP #1)
+// MCP #1
 const int SCK2  = 8;
 const int MOSI2 = 7;
 const int MISO2 = 6;
@@ -21,16 +21,17 @@ const int CS2   = 9;
 #define POT2_IN A2
 #define POT3_IN A3
 
-// aktualna pozycja wipera (0–255)
-int wiperPosition = 128;
+int wiperPositionWhite = 252;                         // start 4.8V
+int wiperPositionBlue = 132;                         // start 2.6
+int wiperPositionPurple = 255 - wiperPositionBlue;  // start 2.4
+int wiperPositionGrey = 126;                       // start 2.4 V
+ 
+int wheelPosition = 0;
 
-// ile kroków na pomiar (reguluje prędkość przesuwu)
-const int stepSize = 1;
-
-// deadzone wokół środka, w której joystick nie powoduje ruchu
+// jaki step na 1 pomiar
+const int step = 1;
 const int deadzone = 15;  
 
-// ----------------------------------------------------------------------
 byte softTransfer(byte data) {
   byte inb = 0;
   for (int i = 7; i >= 0; i--) {
@@ -69,31 +70,42 @@ void setup() {
 
 void loop() {
   gamepad.update();
-  int x = (int)gamepad.getRightX();   // 0…255
+  int x = (int)gamepad.getRightX();   // od 0 do 255
 
-  // środek = 128
   int center = 128;
 
-  // jeżeli powyżej deadzone w prawo → zwiększ
+  // SKRECANIE W PRAWO
   if (x > center + deadzone) {
-    wiperPosition = min(255, wiperPosition + stepSize);
+  
+    wiperPositionBlue =  153; //hyc na 3V 
+    wiperPositionPurple = 255 - wiperPositionBlue;  //hyc na 2V  
+    wheelPosition += step;
+
   }
-  // jeżeli poniżej deadzone w lewo → zmniejsz
+  // SKRECANIE W LEWO
   else if (x < center - deadzone) {
-    wiperPosition = max(0, wiperPosition - stepSize);
+   
+    wiperPositionBlue =  102;   //hyc na 2V
+    wiperPositionPurple = 255 - wiperPositionBlue; //hyc na 3V 
+    wheelPosition -= step;
+
+  } else {
+    wiperPositionBlue = 132;
+    wiperPositionPurple = 255 - wiperPositionBlue;
   }
+  
+  if (wheelPosition > 255)
+      wheelPosition -= 256;
+  else if(wheelPosition < 0)
+      wheelPosition += 256;
 
-  writePotHard(CS0, ADDR_P0, wiperPosition);
-  writePotHard(CS0, ADDR_P1, 255 - wiperPosition);
-  writePotSoft(CS2, ADDR_P0, wiperPosition);
-  writePotSoft(CS2, ADDR_P1, 255 - wiperPosition);
+  wiperPositionWhite = (252 * (abs(wheelPosition - 128))) / 128;
+  wiperPositionGrey = (252* abs(128 - abs(wheelPosition - 64))) /128;
 
-  Serial.print("JoyX="); Serial.print(x);
-  Serial.print("  W="); Serial.print(wiperPosition);
-  Serial.print("  A0="); Serial.print(analogRead(POT0_IN));
-  Serial.print("  A1="); Serial.print(analogRead(POT1_IN));
-  Serial.print("  A2="); Serial.print(analogRead(POT2_IN));
-  Serial.print("  A3="); Serial.println(analogRead(POT3_IN));
+  writePotHard(CS0, ADDR_P0, wiperPositionBlue);
+  writePotHard(CS0, ADDR_P1, wiperPositionPurple);
+  writePotSoft(CS2, ADDR_P0, wiperPositionGrey);
+  writePotSoft(CS2, ADDR_P1, wiperPositionWhite);
 
-  delay(1000 / 60);
+  delay(1000 / 30);
 }
